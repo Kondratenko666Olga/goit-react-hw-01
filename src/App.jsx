809d1f09e4284be87205235311b9ee67a42e412d
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-05-13 15:44:40 +0300

    task 1 styles edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,3 @@
-// import Profile from "./components/Profile/Profile";
-// import userData from "./userData.json";
-
-// function App() {
-//   return (
-//     <>
-//       <div>
-//         <Profile
-//           name={userData.username}
-//           tag={userData.tag}
-//           location={userData.location}
-//           image={userData.avatar}
-//           stats={userData.stats}
-//         />
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import Profile from "./components/Profile/Profile";
 import userData from "./userData.json";
 
